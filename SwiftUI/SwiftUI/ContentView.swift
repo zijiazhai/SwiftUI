3bb264fbe629bf220257576d6124c2da3748cc99
Personal Test <personaltest@Zijia-Zhais-MBP.attlocal.net>
2020-03-26 20:01:42 -0500

    init

---
+++ SwiftUI/SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI
+//
+//  Created by Personal Test on 3/26/20.
+//  Copyright © 2020 Zijia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/SwiftUITests/SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUITests.swift
+//  SwiftUITests
+//
+//  Created by Personal Test on 3/26/20.
+//  Copyright © 2020 Zijia. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI
+
+class SwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
